e8720481dea03e033345cd0a5361bb1bc60ce67b
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-16 16:52:44 +0530

    cleanup the script and use alert window

--- index.js
+++ index.js
@@ -5,60 +5,92 @@ let computerSelection;
 
 const weapons = ["rock", "paper", "scissors"];
 
-// randomize computer choice
+const resultMessages = {
+  playerWins: "You win this round! %s beats %s",
+  computerWins: "You lose this round! %s beats %s",
+  tie: "It's a tie!!",
+};
+
+// Randomize computer choice
 function getComputerChoice() {
-    return weapons[Math.floor(Math.random() * weapons.length)];
+  return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
-// Play one round
-function playRound(playerSelection, computerSelection) {
-    //get Player Selection
+// Determine Round winner
+function determineRoundWinner(playerSelection, computerSelection) {
+  //compare playerSelection and computerSelection and return the winner
+  if (
+    (playerSelection == weapons[0] && computerSelection == weapons[1]) ||
+    (playerSelection == weapons[1] && computerSelection == weapons[2]) ||
+    (playerSelection == weapons[2] && computerSelection == weapons[0])
+  ) {
+    return "computer";
+  } else if (
+    (computerSelection == weapons[0] && playerSelection == weapons[1]) ||
+    (computerSelection == weapons[1] && playerSelection == weapons[2]) ||
+    (computerSelection == weapons[2] && playerSelection == weapons[0])
+  ) {
+    return "player";
+  } else {
+    return "tie";
+  }
+}
 
-    let result;
-
-    if (
-        (playerSelection == weapons[0] && computerSelection == weapons[1]) ||
-        (playerSelection == weapons[1] && computerSelection == weapons[2]) ||
-        (playerSelection == weapons[2] && computerSelection == weapons[0])
-    ) {
-        result = `You lose this round! ${computerSelection} beats ${playerSelection}`;
-        computerScore++;
-    } else if (
-        (computerSelection == weapons[0] && playerSelection == weapons[1]) ||
-        (computerSelection == weapons[1] && playerSelection == weapons[2]) ||
-        (computerSelection == weapons[2] && playerSelection == weapons[0])
-    ) {
-        result = `You win this round! ${playerSelection} beats ${computerSelection}`;
-        playerScore++;
-    } else if (playerSelection == computerSelection) {
-        result = `It's a tie!!`;
-    } else {
-        result = `There is a disturbance in the Force.`;
-    }
-    return result
+// Update scores based on the round winner
+function updateScores(winner) {
+  if (winner === "player") {
+    playerScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  }
 }
 
 function game() {
-    for (let i = 1; i <= 5; i++) {
-        let isValid = false;
-        while (!isValid) {
-            playerSelection = prompt("What is your choice? Rock, Paper or Scissors")
-                .toLowerCase()
-                .trim();
-            isValid = weapons.includes(playerSelection);
-        }
-        computerSelection = getComputerChoice();
-
-        console.log(playRound(playerSelection, computerSelection));
+  for (let i = 1; i <= 5; i++) {
+    // Prompt the player for their choice and validate it
+    let isValid = false;
+    while (!isValid) {
+      playerSelection = prompt("What is your choice? Rock, Paper or Scissors")
+        .toLowerCase()
+        .trim();
+      isValid = weapons.includes(playerSelection);
+      if (!isValid) {
+        alert("Invalid choice. Please enter 'rock', 'paper' or 'scissors'.");
+      }
     }
-    if (playerScore === computerScore) {
-        return `Game Tied!!`
-    } else if (playerScore > computerScore) {
-        return `Player wins the game!`
-    } else if (computerScore > playerScore) {
-        return `Computer wins the game!`
-    }
+    computerSelection = getComputerChoice();
 
+    const roundWinner = determineRoundWinner(
+      playerSelection,
+      computerSelection
+    );
+    updateScores(roundWinner);
+
+    // Display the result of the round
+    if (roundWinner === "player") {
+      alert(
+        resultMessages.playerWins
+          .replace("%s", playerSelection)
+          .replace("%s", computerSelection)
+      );
+    } else if (roundWinner === "computer") {
+      alert(
+        resultMessages.computerWins
+          .replace("%s", computerSelection)
+          .replace("%s", playerSelection)
+      );
+    } else {
+      alert(resultMessages.tie);
+    }
+  }
+  // Determine the overall winner and return the game result
+  if (playerScore === computerScore) {
+    return `Game Tied!! Player: ${playerScore} Computer: ${computerScore}`;
+  } else if (playerScore > computerScore) {
+    return `Player wins the game!! Player: ${playerScore} Computer: ${computerScore}`;
+  } else {
+    return `Computer wins the game!! Player: ${playerScore} Computer: ${computerScore}`;
+  }
 }
 
-console.log(game());
+alert(game());
