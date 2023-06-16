64c0811e3aaae291332068ce1004e1af7555452a
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-06-16 15:00:42 +0530

    add logic to play 5 rounds and return winner

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
-const weapons = ["rock", "paper", "scissors"]
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+let computerSelection;
+
+const weapons = ["rock", "paper", "scissors"];
 
 // randomize computer choice
 function getComputerChoice() {
-    return weapons[Math.floor(Math.random() * weapons.length)]
+    return weapons[Math.floor(Math.random() * weapons.length)];
 }
 
-const computerSelection = getComputerChoice()
-
-//get Player Selection
-let playerSelection;
-let isValid = false;
-
-while (!isValid) {
-    playerSelection = prompt("What is your choice? Rock, Paper or Scissors").toLowerCase().trim()
-    isValid = weapons.includes(playerSelection)
-}
-
-// Play one round 
+// Play one round
 function playRound(playerSelection, computerSelection) {
+    //get Player Selection
 
-    if ((playerSelection == weapons[0] && computerSelection == weapons[1]) || (playerSelection == weapons[1] && computerSelection == weapons[2]) || (playerSelection == weapons[2] && computerSelection == weapons[0])) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`
+    let result;
+
+    if (
+        (playerSelection == weapons[0] && computerSelection == weapons[1]) ||
+        (playerSelection == weapons[1] && computerSelection == weapons[2]) ||
+        (playerSelection == weapons[2] && computerSelection == weapons[0])
+    ) {
+        result = `You lose this round! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
+    } else if (
+        (computerSelection == weapons[0] && playerSelection == weapons[1]) ||
+        (computerSelection == weapons[1] && playerSelection == weapons[2]) ||
+        (computerSelection == weapons[2] && playerSelection == weapons[0])
+    ) {
+        result = `You win this round! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
+    } else if (playerSelection == computerSelection) {
+        result = `It's a tie!!`;
+    } else {
+        result = `There is a disturbance in the Force.`;
     }
-    else if ((computerSelection == weapons[0] && playerSelection == weapons[1]) || (computerSelection == weapons[1] && playerSelection == weapons[2]) || (computerSelection == weapons[2] && playerSelection == weapons[0])) {
-        return `You win! ${playerSelection} beats ${computerSelection}`
-    }
-    else if (playerSelection == computerSelection) {
-        return `It's a tie!!`
-    }
-    else {
-        return `There is a disturbance in the Force.`
-    }
+    return result
 }
 
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        let isValid = false;
+        while (!isValid) {
+            playerSelection = prompt("What is your choice? Rock, Paper or Scissors")
+                .toLowerCase()
+                .trim();
+            isValid = weapons.includes(playerSelection);
+        }
+        computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection))
+        console.log(playRound(playerSelection, computerSelection));
+    }
+    if (playerScore === computerScore) {
+        return `Game Tied!!`
+    } else if (playerScore > computerScore) {
+        return `Player wins the game!`
+    } else if (computerScore > playerScore) {
+        return `Computer wins the game!`
+    }
+
+}
+
+console.log(game());
